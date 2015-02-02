1b68e1a34590d891eefd785e7c5a40588d3c53b8
Aaron Probus <aprobus15@gmail.com>
2015-02-01 20:44:06 -0800

    Adds creation of jwt from claims

--- spec/jerks_whistling_tunes/core_spec.clj
+++ spec/jerks_whistling_tunes/core_spec.clj
@@ -4,7 +4,18 @@
 
 (def jwt "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEyMzQ1Njc4OTAsIm5hbWUiOiJKb2huIERvZSIsImFkbWluIjp0cnVlfQ.eoaDVGTClRdfxUZXiPs3f8FmJDkDE_VCQFXqKxpLsts")
 
+(def secret "my secret")
+
 (describe "jerks-whistling-tunes.core"
+  (describe "sign"
+    (it "signs empty claims"
+      (should= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.e30.bYbzTu1BlXUhf-V8G0JCR2yarRl9J7fzZvssGRentYY"
+               (sign "HS256" secret {})))
+
+    (it "signs simple claim"
+      (should= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJmb28ifQ.A2MH3GUuTTdUaFab1h8-ZxKGBEeVLfeIT-RA__SgLkY"
+               (sign "HS256" secret {:iss "foo"}))))
+
   (describe "valid?"
     (it "verfies token"
       (should (valid? "secret" jwt)))
--- src/jerks_whistling_tunes/core.clj
+++ src/jerks_whistling_tunes/core.clj
@@ -5,23 +5,28 @@
 (java.security.Security/addProvider
  (org.bouncycastle.jce.provider.BouncyCastleProvider.))
 
-(def base-64 (org.apache.commons.codec.binary.Base64. true))
+(defn- first-arg [& args]
+  (first args))
+
+(def ^:private base-64 (org.apache.commons.codec.binary.Base64. true))
 (defn- decode-64 [str]
   (.decode base-64 str))
+(defn- encode-64 [bytes]
+  (let [result (String. (.encode base-64 bytes) "UTF-8")
+        len-without-newline (- (count result) 2)]
+    (.substring result 0 len-without-newline)))
 
-(defmulti sign (fn [& args] (first args)))
-(defmethod sign "HS256" [_ secret body]
+(defmulti create-signature first-arg)
+(defmethod create-signature "HS256" [_ secret body]
   (let [hmac-key (javax.crypto.spec.SecretKeySpec. (.getBytes secret "UTF-8") "HS256")
         encoder (doto (javax.crypto.Mac/getInstance "HmacSHA256")
                   (.init hmac-key)
-                  (.update (.getBytes body "UTF-8")))
-        signature (String. (.encode base-64 (.doFinal encoder)) "UTF-8")
-        adjusted-length (- (count signature) 2)]
-    (.substring signature 0 adjusted-length)))
+                  (.update (.getBytes body "UTF-8")))]
+    (encode-64 (.doFinal encoder))))
 
 (defmulti verify (fn [& args] (first args)))
 (defmethod verify "HS256" [_ secret body signature]
-  (let [gen-sig (sign "HS256" secret body)]
+  (let [gen-sig (create-signature "HS256" secret body)]
     (eq? signature gen-sig)))
 
 (defn- parse-segment [segment]
@@ -37,3 +42,14 @@
             body (str header-str "." claims-str)]
         (verify (:alg header) secret body sig-str))
       false)))
+
+(defn- create-segment [segment]
+  (let [json-str (json/write-str segment)]
+    (encode-64 (.getBytes json-str "UTF-8"))))
+
+(defn sign [alg secret claims]
+  (let [header {:alg alg
+                :typ "JWT"}
+        body (str (create-segment header) "." (create-segment claims))
+        signature (create-signature alg secret body)]
+    (str body "." signature)))
