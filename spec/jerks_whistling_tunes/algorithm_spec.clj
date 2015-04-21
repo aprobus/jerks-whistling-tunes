2b67cd649e5ec45618285e8dee98b8ea359007ca
Aaron Probus <pair+aaron@fundingcircle.com>
2015-04-21 16:29:59 -0700

    Adds HS384 and HS512 support

--- spec/jerks_whistling_tunes/algorithm_spec.clj
+++ spec/jerks_whistling_tunes/algorithm_spec.clj
@@ -10,6 +10,16 @@
   (describe "hs256"
     (it "encodes the string"
       (should= "eoaDVGTClRdfxUZXiPs3f8FmJDkDE_VCQFXqKxpLsts"
-               ((hs256 secret) unsigned-jwt)))))
+               ((hs256 secret) unsigned-jwt))))
+
+  (describe "hs384"
+    (it "encodes the string"
+      (should= "Wk-GxVm14_MHtn7esykHAFOUwob1Z8PXMdvFbnAh2JnluyV6KAe6NEHZXG-8ZZDT"
+               ((hs384 secret) unsigned-jwt))))
+
+  (describe "hs512"
+    (it "encodes the string"
+      (should= "0JRoNr4i-fz-Pz0w0No331-_8pq9wnSM3Ic5W9dZOCT8gFTfu8bJpd8nV5ELUonPY1fOnIQScKeA\r\ncc8niKLK3w"
+               ((hs512 secret) unsigned-jwt)))))
 
 (run-specs)
--- src/jerks_whistling_tunes/algorithm.clj
+++ src/jerks_whistling_tunes/algorithm.clj
@@ -4,12 +4,16 @@
 
 (def ^:private byte-array-class (Class/forName "[B"))
 
-(defn hs256 [secret]
+(defn- create-hmac-signer [secret-type instance-type secret]
   (let [secret-bytes (convert secret byte-array-class)
-        hmac-key (javax.crypto.spec.SecretKeySpec. secret-bytes "HS256")]
+        hmac-key (javax.crypto.spec.SecretKeySpec. secret-bytes secret-type)]
     (fn [body]
       (let [body-bytes (convert body byte-array-class)
-            encoder (doto (javax.crypto.Mac/getInstance "HmacSHA256")
+            encoder (doto (javax.crypto.Mac/getInstance instance-type)
                       (.init hmac-key)
                       (.update body-bytes))]
         (utils/encode-base-64 (.doFinal encoder))))))
+
+(def hs256 (partial create-hmac-signer "HS256" "HmacSHA256"))
+(def hs384 (partial create-hmac-signer "HS384" "HmacSHA384"))
+(def hs512 (partial create-hmac-signer "HS512" "HmacSHA512"))
