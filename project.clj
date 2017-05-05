49f95d7e88fa26f9b1239083c82637fb93517c08
Aaron Probus <aaron@fundingcircle.com>
2017-05-04 22:34:36 -0700

    0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jerks-whistling-tunes "0.2.4"
+(defproject jerks-whistling-tunes "0.3.0"
   :description "Clojure library for creating/verifying JWT"
   :url "https://github.com/aprobus/jerks-whistling-tunes"
   :license {:name "Eclipse Public License"
@@ -11,5 +11,7 @@
                  [commons-codec/commons-codec "1.10"]]
   :profiles {:dev {:dependencies [[speclj "3.3.2"]
                                   [org.bouncycastle/bcpkix-jdk15on "1.56"]]}}
+  :repositories [["releases" {:url "https://clojars.org/repo"
+                              :sign-releases false }]]
   :plugins [[speclj "3.3.2"]]
   :test-paths ["spec"])
