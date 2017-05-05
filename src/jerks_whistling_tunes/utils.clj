5c932e0bf73e3a8cd2fe0aea1067c1e5bc365cf8
jeaye <contact@jeaye.com>
2017-05-05 11:08:21 -0700

    Remove reflection from all namespaces (34% faster)
    Before
    
    ```text
    Evaluation count : 7374 in 6 samples of 1229 calls.
                 Execution time mean : 80.742711 µs
        Execution time std-deviation : 570.797426 ns
       Execution time lower quantile : 80.096170 µs ( 2.5%)
       Execution time upper quantile : 81.605858 µs (97.5%)
                       Overhead used : 6.696816 ns
    ```
    
    After
    
    ```text
    Evaluation count : 11454 in 6 samples of 1909 calls.
                 Execution time mean : 53.017780 µs
        Execution time std-deviation : 665.405999 ns
       Execution time lower quantile : 52.061622 µs ( 2.5%)
       Execution time upper quantile : 53.609765 µs (97.5%)
                       Overhead used : 6.763715 ns
    ```

--- src/jerks_whistling_tunes/utils.clj
+++ src/jerks_whistling_tunes/utils.clj
@@ -1,14 +1,14 @@
 (ns jerks-whistling-tunes.utils
   (:import org.apache.commons.codec.binary.Base64))
 
-(def ^:private base-64-encoder (Base64. -1 (.getBytes "") true))
+(def ^:private ^Base64 base-64-encoder (Base64. -1 (.getBytes "") true))
 
 (defn decode-base-64
   "Returns the decoded base 64 url safe encoded string."
-  [str]
-  (.decode base-64-encoder str))
+  [s]
+  (.decode base-64-encoder s))
 
 (defn encode-base-64
   "Returns a base 64 url safe encoded representation of the string"
-  [bytes]
-  (String. (.encode base-64-encoder bytes) "UTF-8"))
+  [^bytes bs]
+  (String. (.encode base-64-encoder bs) "UTF-8"))
