c70cfd1820ec58c2abd32ef88a35c8530bec14df
jeaye <contact@jeaye.com>
2017-05-18 16:41:16 -0700

    Update deps
    This includes a long-awaits byte-streams fix
    https://github.com/ztellman/byte-streams/pull/27

--- project.clj
+++ project.clj
@@ -5,12 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.json "0.2.6"]
-                 [org.bouncycastle/bcprov-jdk15on "1.56"]
-                 [byte-streams "0.2.2"]
+                 [org.bouncycastle/bcprov-jdk15on "1.57"]
+                 [byte-streams "0.2.3"]
                  [crypto-equality "1.0.0"]
                  [commons-codec/commons-codec "1.10"]]
   :profiles {:dev {:dependencies [[speclj "3.3.2"]
-                                  [org.bouncycastle/bcpkix-jdk15on "1.56"]]
+                                  [org.bouncycastle/bcpkix-jdk15on "1.57"]]
                    :global-vars {*warn-on-reflection* true}}}
   :repositories [["releases" {:url "https://clojars.org/repo"
                               :sign-releases false }]]
